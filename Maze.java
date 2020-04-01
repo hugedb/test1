f39873a5e009b069d863fc1a51558fa90330c444
NING ZHANG <ning.zhang4@student.curtin.edu.au>
2020-04-01 11:55:45 +0800

    update 2

--- Maze.java
+++ Maze.java
@@ -16,7 +16,7 @@ public class Maze
     public static void main(String[] args)
     {
         int[][] grid = {
-            { WALL,  WALL,  EMPTY,  WALL,  EMPTY,  WALL,  WALL,  WALL,  WALL, WALL},
+            { WALL,  EMPTY,  EMPTY,  WALL,  EMPTY,  WALL,  WALL,  WALL,  WALL, WALL},
             {START, EMPTY,  EMPTY,  EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, WALL},
             { WALL, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,  WALL, EMPTY,  WALL, WALL},
             { WALL,  WALL,  WALL,  WALL, EMPTY,  WALL, EMPTY,  WALL, EMPTY, WALL},
